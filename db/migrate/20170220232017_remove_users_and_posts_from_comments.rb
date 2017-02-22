b3c524226b7d9b4f50c1655f340cb02a778a4870
Bin Tran <sudocode.bin@gmail.com>
2017-02-22 01:46:04 -0800

    before deployment

--- db/migrate/20170220232017_remove_users_and_posts_from_comments.rb
+++ db/migrate/20170220232017_remove_users_and_posts_from_comments.rb
@@ -1,6 +1,6 @@
 class RemoveUsersAndPostsFromComments < ActiveRecord::Migration[5.0]
   def change
-    remove_column :comments, :user
-    remove_column :comments, :post
+    remove_column :comments, :user, index: true, foreign_key: true
+    remove_column :comments, :post, index: true, foreign_key: true
   end
 end
